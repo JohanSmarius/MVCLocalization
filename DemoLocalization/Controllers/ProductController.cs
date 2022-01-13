52d87b0c08655cb5b7833294955e3a362cbd2d9f
johansmarius <jsmarius@hotmail.com>
2022-01-13 17:27:54 +0100

    Updated the layout

--- DemoLocalization/Controllers/ProductController.cs
+++ DemoLocalization/Controllers/ProductController.cs
@@ -1,12 +1,55 @@
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using DemoLocalization.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoLocalization.Controllers;
 
-public class Product : Controller
+public class ProductController : Controller
 {
     // GET
     public IActionResult Index()
     {
-        return View();
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("nl-NL");
+
+        Product product;
+        if (TempData.ContainsKey("NewProduct"))
+        {
+            string productData = TempData["NewProduct"].ToString();
+            product = JsonSerializer.Deserialize<Product>(productData);
+        }
+        else
+        {
+            product = new Product
+            {
+                Name = "Dogfood",
+                PictureUrl = "https://cdn.webshopapp.com/shops/61259/files/104272172/nature-dogfood-nature-dogfood-graanvrije-hondenbro.jpg",
+                Price = 11.89m
+            };    
+        }
+        
+        return View(product);
+        
+    }
+
+    [HttpGet]
+    public IActionResult NewProduct()
+    {
+        var emptyModel = new Product();
+        return View(emptyModel);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult NewProduct(Product product)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+
+        TempData["NewProduct"] = JsonSerializer.Serialize(product);
+        return RedirectToAction("Index");
     }
 }
--- DemoLocalization/Models/Product.cs
+++ DemoLocalization/Models/Product.cs
@@ -1,6 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DemoLocalization.Models;
 
 public class Product
 {
-    \
+    [Required]
+    public string Name { get; set; }
+    
+    [DataType(DataType.Url)]
+    public string PictureUrl { get; set; }
+
+    [DataType(DataType.Currency)]
+    public decimal Price { get; set; }
 }
